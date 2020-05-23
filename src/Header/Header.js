7ab7c512221dcdbe2dfdf18e737f7a44ca3e7855
Jan Pozivil <crs1138@me.com>
2020-05-23 14:12:13 +0200

    🌟 Add Twitter meta
    Because:
    ---
    * add Twitter meta data to the header of the HTML

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,18 +5,10 @@ import { Helmet } from 'react-helmet';
 
 
 function CVHeader({ title, description }) {
-    const ogDesc = `A chronological record of how the UK dealt with a pandemic - with sources to all news articles in links`;
     return (
         <header className={styles.cv19__header}>
             <Helmet>
                 <title>{title}</title>
-                <meta property="og:title" content={title} />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://crs1138.github.io/cv19-uk-timeline/" />
-                <meta property="og:image" content="https://crs1138.github.io/cv19-uk-timeline/cv19-header-openGraph.jpg" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta name="description" content={ogDesc}/>
             </Helmet>                
             <div className="container">
                 <h1 className={styles.cv19__title}>{ title }</h1>
